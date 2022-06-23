1e138dd045ad91a0f963cf3b532f3f48643e07eb
fernandonunex <fer.bsof@outlook.com>
2022-06-22 21:28:13 -0500

    Recursive function

---
+++ pyramid_sum.py
@@ -0,0 +1,23 @@
+"""
+Code used during the class 'Operaciones esenciales en colecciones
+"""
+
+
+def pyramid_sum(lower, upper, margin=0):
+    """Return the sum of the numbers from lower to upper,
+    and outputs a trace of the arguments and return values 
+    on each call"""
+    blanks = " " * margin
+    print(blanks, lower, upper)
+
+    if lower > upper:
+        print(blanks, 0)
+        return 0
+    else:
+        result = lower + pyramid_sum(lower+1, upper, margin + 4)
+        print(blanks, result)
+        return result
+    
+if __name__ == "__main__":
+        pyramid_sum(1,8)
+        
