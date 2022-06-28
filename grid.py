156ed9ccb6b324dab20b10d6b53a03abcd4558a7
fernandonunex <fer.bsof@outlook.com>
2022-06-28 17:04:08 -0500

    Adding a little documentation to grid.py and node.py

--- grid.py
+++ grid.py
@@ -1,25 +1,34 @@
+"""Module to create an array 2 dimensional"""
+
 from array_ import Array
 
 class Grid():
-
+    """Array of 2 dimensions"""
     def __init__(self, rows, columns, fill_value=None):
         self.data = Array(rows)
         for row in range(rows):
             self.data[row] = Array(columns, fill_value)
 
     def __len__(self):
+        """Return the len of rows"""
         return len(self.data)
 
     def get_height(self):
+        """Return the len of rows"""
         return len(self.data)
 
     def get_width(self):
+        """Return the len of columns"""
         return len(self.data[0])
 
     def __getitem__(self, index):
+        """Return the an item given a index"""
         return self.data[index]
 
     def __str__(self):
+        """Representation of the data in str in the 
+        data structure 
+        """
         result = ""
 
         for row in range(self.get_height()):
--- node.py
+++ node.py
@@ -1,6 +1,8 @@
-
+"""Module to model nodes"""
 
 class Node():
+    """Structure of a node, has data and the next reference
+    to another node."""
     def __init__(self, data, next=None):
         self.data = data
         self.next = next
