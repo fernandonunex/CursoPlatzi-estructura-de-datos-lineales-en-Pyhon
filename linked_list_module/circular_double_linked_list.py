115ded479fd1bc039155f7c434356aa70c25dfb3
fernandonunex <fer.bsof@outlook.com>
2022-07-06 15:21:37 -0500

    Implementig methods to get the nodes by index and by data, and also a little test about it

--- linked_list_module/circular_double_linked_list.py
+++ linked_list_module/circular_double_linked_list.py
@@ -54,6 +54,60 @@ class CircleDoubleLinkedList():
                 yield probe.data
                 probe = probe.next
             yield probe.data
+    
+    def reverse_iter(self):
+        """Return the data of every node form tail to head"""
+        if self.tail == None:
+            return print("The circular double linked list is empty")
+        else:
+            probe = self.tail
+
+            while probe != self.head:
+                yield probe.data
+                probe = probe.previous
+            yield probe.data
+    
+    def _get_node_by_data(self, data):
+        """Search an item by the data stored in it, starting from the head to tail
+            Makes match with the first item found
+            If is found then will return the pointer"""
+        probe = self.head
+        for _ in range(int(self.size())):
+            if probe.data == data: # if the data is found, then return the pointer
+                print(f"Data found {data}")
+                return probe
+            else:
+                probe = probe.next
+        
+        probe = None # if the data is not found then return a pointer of None
+        print(f"Data don't found {data}")
+
+        return probe
+    
+    def _get_node_by_index(self, index):
+        """Get a node by index, start counting from 0 (is the head) and
+            finish in n (is the tail)
+            If index > self._size then return the self.tail 
+            If index < 0 then return the self.head
+        """
+        # check out if the index is bigger than the size of the circular double list
+        if index >= int(self.size()):
+            probe = self.tail
+            return probe
+
+        elif index <= 0:
+            probe = self.head
+            return probe
+
+        else:
+
+            probe = self.head # start at the head and is the index 0
+            for _ in range(index):
+                probe = probe.next
+            return probe
+
+
+
 
     def size(self):
         """Return the size of the SinglyLinkedList"""
--- linked_list_module/testing_double_linked_list.py
+++ linked_list_module/testing_double_linked_list.py
@@ -19,10 +19,36 @@ circular.append(5)
 circular.size()
 circular.append(2)
 circular.append(2)
-circular.append(2)
-circular.size()
+circular.append('aloa')
+
+print(f"size{circular.size()}")
 
 # print(circular.head.next.data)
 
 for data in circular.iter():
     print(data)
+
+# print("**"*10)
+# for data in circular.reverse_iter():
+#     print(data)
+
+print("**"*10)
+# pointer_of_data = circular._get_node_by_data('aloa')
+# print(pointer_of_data.data)
+
+pointer_of_data = circular._get_node_by_index(0)
+print(pointer_of_data.data)
+pointer_of_data = circular._get_node_by_index(1)
+print(pointer_of_data.data)
+pointer_of_data = circular._get_node_by_index(2)
+print(pointer_of_data.data)
+pointer_of_data = circular._get_node_by_index(3)
+print(pointer_of_data.data)
+pointer_of_data = circular._get_node_by_index(4)
+print(pointer_of_data.data)
+pointer_of_data = circular._get_node_by_index(2)
+print(pointer_of_data.data)
+pointer_of_data = circular._get_node_by_index(5)
+print(pointer_of_data.data)
+pointer_of_data = circular._get_node_by_index(1)
+print(pointer_of_data.data)
