cea380466207b73469258e0baf43aa8205ae0a02
fernandonunex <fer.bsof@outlook.com>
2022-07-10 17:02:03 -0500

    [C20] Queue basada en dos stacks, and test the methods

--- queue/testing_list_based_queue.py
+++ queue/testing_list_based_queue.py
@@ -1,15 +1,54 @@
 from list_based_queue import ListQueue
+from stack_based_queue import Queue
 
-food = ListQueue()
-food.enqueue('taco')
-food.enqueue('arroz con leche')
-food.enqueue('ice')
-print(food.dequeue())
-print(food.dequeue())
-print(food.dequeue())
+# food = ListQueue()
+# food.enqueue('taco')
+# food.enqueue('arroz con leche')
+# food.enqueue('ice')
+# print(food.dequeue())
+# print(food.dequeue())
+# print(food.dequeue())
 
-print("--"*10)
+# print("--"*10)
 #food.traverse()
+
+
+# Testing stack based queue
+
+# checando como se comportan ambos stacks
+numbers = Queue()
+numbers.enqueue(5)
+numbers.enqueue(6)
+numbers.enqueue(7)
+numbers.enqueue(8)
+print(numbers.inboud_stack)
+print(numbers.outbound_stack)
+print(numbers.dequeue())
+print(numbers.inboud_stack)
+print(numbers.outbound_stack)
+numbers.enqueue(9)
+numbers.enqueue(10)
+print(numbers.inboud_stack)
+print(numbers.outbound_stack)
+print(numbers.dequeue())
+print(numbers.inboud_stack)
+print(numbers.outbound_stack)
+print(numbers.dequeue())
+print(numbers.inboud_stack)
+print(numbers.outbound_stack)
+print(numbers.dequeue())
+print(numbers.inboud_stack)
+print(numbers.outbound_stack)
+print(numbers.dequeue())
+print(numbers.inboud_stack)
+print(numbers.outbound_stack)
+
+
+
+
+
+
+
 
 
 
