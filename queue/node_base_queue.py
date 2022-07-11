7b6990cc1cced5932972b5509d23ba1a78c92456
fernandonunex <fer.bsof@outlook.com>
2022-07-11 15:58:35 -0500

    [C21] create a queue based on nodes

---
+++ queue/node_base_queue.py
@@ -0,0 +1,47 @@
+"""Module to model nodes"""
+
+class Node():
+    """Structure of a node, has data and the next reference
+    to another node."""
+    def __init__(self, data, next=None):
+        self.data = data
+        self.next = next
+
+
+class TwoWayNode(Node):
+    def __init__(self, data, previous=None, next=None):
+        super().__init__(data, next)
+        self.previous = previous
+
+class Queue_node:
+    def __init__(self) -> None:
+        self.head = None
+        self.tail = None
+        self.count = 0
+
+    def enqueue(self, data):
+        new_node = TwoWayNode(data, None, Node)
+        if self.head is None:
+            self.head = new_node
+            self.tail = self.head # two ways
+
+        else: 
+            new_node.previous = self.tail
+            self.tail.next = new_node
+            self.tail = new_node
+        
+        self.count += 1
+
+    def dequeue(self):
+        current = self.head
+        
+        if self.count == 1:
+            self.count -= 1
+            self.head = None
+            self.tail = None
+        elif self.count > 1:
+            self.head = self.head.next
+            self.head.previous = None
+            self.count -= 1
+
+        return current.data 
--- queue/testing_list_based_queue.py
+++ queue/testing_list_based_queue.py
@@ -16,32 +16,51 @@ from stack_based_queue import Queue
 # Testing stack based queue
 
 # checando como se comportan ambos stacks
-numbers = Queue()
-numbers.enqueue(5)
-numbers.enqueue(6)
-numbers.enqueue(7)
-numbers.enqueue(8)
-print(numbers.inboud_stack)
-print(numbers.outbound_stack)
-print(numbers.dequeue())
-print(numbers.inboud_stack)
-print(numbers.outbound_stack)
-numbers.enqueue(9)
-numbers.enqueue(10)
-print(numbers.inboud_stack)
-print(numbers.outbound_stack)
-print(numbers.dequeue())
-print(numbers.inboud_stack)
-print(numbers.outbound_stack)
-print(numbers.dequeue())
-print(numbers.inboud_stack)
-print(numbers.outbound_stack)
-print(numbers.dequeue())
-print(numbers.inboud_stack)
-print(numbers.outbound_stack)
-print(numbers.dequeue())
-print(numbers.inboud_stack)
-print(numbers.outbound_stack)
+# numbers = Queue()
+# numbers.enqueue(5)
+# numbers.enqueue(6)
+# numbers.enqueue(7)
+# numbers.enqueue(8)
+# print(numbers.inboud_stack)
+# print(numbers.outbound_stack)
+# print(numbers.dequeue())
+# print(numbers.inboud_stack)
+# print(numbers.outbound_stack)
+# numbers.enqueue(9)
+# numbers.enqueue(10)
+# print(numbers.inboud_stack)
+# print(numbers.outbound_stack)
+# print(numbers.dequeue())
+# print(numbers.inboud_stack)
+# print(numbers.outbound_stack)
+# print(numbers.dequeue())
+# print(numbers.inboud_stack)
+# print(numbers.outbound_stack)
+# print(numbers.dequeue())
+# print(numbers.inboud_stack)
+# print(numbers.outbound_stack)
+# print(numbers.dequeue())
+# print(numbers.inboud_stack)
+# print(numbers.outbound_stack)
+
+
+#TESTING QUEUES BASED IN NODES
+
+from node_base_queue import Queue_node
+
+food = Queue_node()
+food.enqueue('gorditas')
+food.enqueue('carne asada')
+food.enqueue('ensalada')
+
+print(food.head.next.data)
+print(food.tail.data)
+print(food.tail.previous.data)
+print(food.count)
+print(food.dequeue())
+print(food.head.data)
+
+
 
 
 
